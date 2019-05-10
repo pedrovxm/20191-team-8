70b8e80dc868413ca94146b6ad2e258e4b9b7960
gprimate <42354236+gprimate@users.noreply.github.com>
2019-05-10 14:19:10 -0300

    Add files via upload

---
+++ BANG!/src/renegado.cpp
@@ -0,0 +1,9 @@
+//
+// Created by gprimate on 10/05/19.
+//
+
+#include "renegado.h"
+
+void Renegado::check_win() {
+    std::cout << "Funcao nao implementada" << std::endl;
+}
---
+++ BANG!/src/xerife.cpp
@@ -0,0 +1,9 @@
+//
+// Created by gprimate on 10/05/19.
+//
+
+#include "xerife.h"
+
+void Xerife::check_win() {
+    std::cout << "Funcao nao implementada" << std::endl;
+}
