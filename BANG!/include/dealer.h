8656f5611e4c1c7925fcc9647925682406e14509
Adalberto <adalbertob.vieira@gmail.com>
2019-05-11 15:49:33 -0300

    deck and joagador declaration

---
+++ BANG!/include/dealer.h
@@ -0,0 +1,3 @@
+#ifndef TP_DEALER_H
+#define TP_DEALER_H
+#endif
---
+++ BANG!/src/jogador.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <exception>
+#include "joagador.h"
+Jogador(Papel role, Personagem persona, vector<Carta> hand, int id){
+    throw "Funcao nao implementada";
+}
+~Jogador(){
+    throw "Funcao nao implementada";
+}
+void set_id(int id){
+    throw "Funcao nao implementada";
+}
+void set_life(int life){
+    throw "Funcao nao implementada";
+}
+void set_range(int range){
+    throw "Funcao nao implementada";
+}
+void set_hand(std::vector<carta> hand){
+    throw "Funcao nao implementada";
+}
+int get_id(){
+    throw "Funcao nao implementada";
+}
+int get_life(){
+    throw "Funcao nao implementada";
+}
+int get_role(){
+    throw "Funcao nao implementada";
+}
+int get_range(){
+    throw "Funcao nao implementada";
+}
+Papel get_papel(){
+    throw "Funcao nao implementada";
+}
+Personagem get_persona(){
+    throw "Funcao nao implementada";
+}
+std::vector<Carta> get_hand(){
+    throw "Funcao nao implementada";
+}
+void heal(){
+    throw "Funcao nao implementada";
+}
+void sufer_damage(){
+    throw "Funcao nao implementada";
+}
+void add_carta(Carta carta){
+    throw "Funcao nao implementada";
+}
