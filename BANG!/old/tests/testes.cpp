425ccc2ad6d0e53033dd43e73f7e73830b14bb55
Alan Borges <alan@phari.solutions>
2019-06-08 01:37:19 -0300

    Unit tests for personagens

--- BANG!/old/tests/testes.cpp
+++ BANG!/old/tests/testes.cpp
@@ -11,7 +11,11 @@
 #include <string>
 #include <math.h>
 
-TEST_CASE("01 - Testando construção de classes") {
+/////////////////
+// Personagens //
+/////////////////
+
+TEST_CASE("01.01 - Testando construção de classes para personagens") {
   CHECK_NOTHROW(RoseDoolan(4, "image_path"));
   CHECK_NOTHROW(PedroRamirez(3, "image_path"));
   CHECK_NOTHROW(BartCassidy(5, "image_path"));
@@ -22,23 +26,53 @@ TEST_CASE("01 - Testando construção de classes") {
   CHECK_NOTHROW(SuzyLafayette(3, "image_path"));
 }
 
-// TEST_CASE("02 - Acessando métodos das classes base") {
-//     Triangulo t("azul", 6, 7, 8);
-//     CHECK((abs(t.get_area()-20.3331625676))<0.0001);
-//     CHECK(t.get_perimetro() == 21);
-//
-//     Quadrado q("verde", 4);
-//     CHECK(q.get_area() == 16);
-//     CHECK(q.get_perimetro() == 16);
-//
-//     Cubo c("roxo", 4);
-//     CHECK(c.get_volume() == 64);
-//
-//     Esfera e("amarelo", 3);
-//     CHECK(abs(e.get_volume()-113.097)<0.01);
-//
-//     Circulo cr("rosa", 2);
-//     CHECK(abs(cr.get_area()-12.5664)<0.001);
-//     CHECK(abs(cr.get_perimetro()-12.5664)<0.001);
-//
-// }
+TEST_CASE("01.02 - Acessando métodos das classes de personagens") {
+  RoseDoolan rose_doolan(4, "image_path");
+  CHECK(rose_doolan.get_vida() == 4);
+  CHECK(rose_doolan.get_imagem() == "image_path");
+  CHECK(rose_doolan.get_nome() == "Rose Doolan");
+  CHECK(rose_doolan.get_descricao() == "Texto descritivo do personagem");
+  // CHECK(); // Efeito
+
+  PedroRamirez pedro_ramirez(4, "image_path");
+  CHECK(pedro_ramirez.get_vida() == 4);
+  CHECK(pedro_ramirez.get_imagem() == "image_path");
+  CHECK(pedro_ramirez.get_nome() == "Pedro Ramirez");
+  CHECK(pedro_ramirez.get_descricao() == "Texto descritivo do personagem");
+  // CHECK(); // Efeito
+
+  BartCassidy bart_cassidy(4, "image_path");
+  CHECK(bart_cassidy.get_vida() == 4);
+  CHECK(bart_cassidy.get_imagem() == "image_path");
+  CHECK(bart_cassidy.get_nome() == "Bart Cassidy");
+  CHECK(bart_cassidy.get_descricao() == "Texto descritivo do personagem");
+  // CHECK(); // Efeito
+
+  WillyKid willy_kid(4, "image_path");
+  CHECK(willy_kid.get_vida() == 4);
+  CHECK(willy_kid.get_imagem() == "image_path");
+  CHECK(willy_kid.get_nome() == "Willy Kid");
+  CHECK(willy_kid.get_descricao() == "Texto descritivo do personagem");
+  // CHECK(); // Efeito
+
+  ElGringo el_gringo(4, "image_path");
+  CHECK(el_gringo.get_vida() == 4);
+  CHECK(el_gringo.get_imagem() == "image_path");
+  CHECK(el_gringo.get_nome() == "El Gringo");
+  CHECK(el_gringo.get_descricao() == "Texto descritivo do personagem");
+  // CHECK(); // Efeito
+
+  BlackJack black_jack(4, "image_path");
+  CHECK(black_jack.get_vida() == 4);
+  CHECK(black_jack.get_imagem() == "image_path");
+  CHECK(black_jack.get_nome() == "Black Jack");
+  CHECK(black_jack.get_descricao() == "Texto descritivo do personagem");
+  // CHECK(); // Efeito
+
+  SuzyLafayette suzy_lafayette(4, "image_path");
+  CHECK(suzy_lafayette.get_vida() == 4);
+  CHECK(suzy_lafayette.get_imagem() == "image_path");
+  CHECK(suzy_lafayette.get_nome() == "Suzy Lafayette");
+  CHECK(suzy_lafayette.get_descricao() == "Texto descritivo do personagem");
+  // CHECK(); // Efeito
+}
